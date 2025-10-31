dc810b25fa4acf77db3ac25d798970a640621c54
Favour Otaye <favourotaye7@gmail.com>
2025-10-31 09:03:06 -0700

    Combined WelcomeMessage with Header, MainContent and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <WelcomeMessage />
       <MainContent />
       <Footer />
     </div>
