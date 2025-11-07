fd7466c51eae07d0854d29e5502991841756abba
Favour Otaye <favourotaye7@gmail.com>
2025-11-07 05:25:21 -0800

    fix: corrected folder paths and Context API structure

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
-import { useContext } from 'react';
-import UserContext from '../UserContext';
+import { useContext } from "react";
+import UserContext from "../UserContext";
 
 function UserProfile() {
   const user = useContext(UserContext);
 
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue' }}>{user.name}</h2>
+    <div>
+      <h2>{user.name}</h2>
       <p>Email: {user.email}</p>
     </div>
   );
