240a706c689aa8c70d89c3c308614fdca7502015
Favour Eloghosahumwen Otaye <favourotaye7@gmail.com>
2025-11-07 14:14:56 -0800

    Replace UserProfile with UserDetails in UserInfo

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import UserProfile from "./UserProfile";
+import UserDetails from "./UserDetails";
 
 function UserInfo() {
-  return <UserProfile />;
+  return <UserDetails />;
 }
 
 export default UserInfo;
