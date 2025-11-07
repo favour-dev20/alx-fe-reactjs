61e6a7eff5b3275adbdbbdf8d20e487af3972d71
Favour Eloghosahumwen Otaye <favourotaye7@gmail.com>
2025-11-07 07:35:25 -0800

    Add defensive check for user data in UserDetails
    Added a defensive check to handle cases where user data may not be available.

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,8 +1,13 @@
-import { useContext } from "react";
-import UserContext from "./UserContext";
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
+
+  // Defensive check in case context is not provided
+  if (!userData) {
+    return <div>No user data available</div>;
+  }
 
   return (
     <div>
