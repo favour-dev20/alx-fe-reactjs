d9c44b4fdc7ed103bd8c69bc3a492dff26ecb635
Favour Otaye <favourotaye7@gmail.com>
2025-11-07 04:48:35 -0800

    refactor: replaced prop drilling with Context API

---
+++ alx-react-app-props/src/UserDetails.jsx
@@ -0,0 +1,20 @@
+// src/UserDetails.jsx
+import { useContext } from 'react';
+import { UserContext } from './UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  if (!userData) {
+    return <div>No user data available.</div>;
+  }
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
