443f07189fb88df7ebbc9c804f6ac78ceec38ffa
Favour Eloghosahumwen Otaye <favourotaye7@gmail.com>
2025-11-07 07:41:30 -0800

    Refactor UserContext.js import and export comments
    Updated import statement to use single quotes and added comment for export.

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
+// Create a Context for the user data
 const UserContext = createContext(null);
 
+// Export the context so it can be imported in other files
 export default UserContext;
