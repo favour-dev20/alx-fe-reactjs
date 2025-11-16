dd94ff2c842c4b245960760674a425a7be755094
Favour Eloghosahumwen Otaye <favourotaye7@gmail.com>
2025-11-16 02:14:38 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import RecipeList from './components/RecipeList'
-import AddRecipeForm from './components/AddRecipeForm'
-import RecipeDetails from './components/RecipeDetails'
-import EditRecipeForm from './components/EditRecipeForm'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import RecipeDetail from "./components/RecipeDetail";
+import EditRecipeForm from "./components/EditRecipeForm";
 
 function App() {
   return (
-    <div style={{ maxWidth: 600, margin: '24px auto', padding: '0 16px' }}>
-      <h1 style={{ textAlign: 'center' }}>Recipe Sharing App</h1>
-      <AddRecipeForm />
+    <Router>
       <Routes>
         <Route path="/" element={<RecipeList />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
         <Route path="/edit/:id" element={<EditRecipeForm />} />
       </Routes>
-    </div>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
