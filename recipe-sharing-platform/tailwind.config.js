75c2055e7ac18925d84e5f3ce9575379360cea96
Favour Otaye <favourotaye7@gmail.com>
2025-12-06 00:17:49 -0800

    fix: correct Tailwind content paths for ALX review

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src//*.{js,jsx,ts,tsx}"], // fix the path
-  darkMode: false, // or 'media' or 'class' if you plan to use dark mode
+  content: ["./public/index.html", "./src//*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
