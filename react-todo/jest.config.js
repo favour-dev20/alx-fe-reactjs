81dcc1b1e1691a90f4e1715f0447a4735f025021
Favour Otaye <favourotaye7@gmail.com>
2025-12-14 11:44:08 -0800

    Implement TodoList component with Jest tests

---
+++ react-todo/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  testMatch: ['/_tests_//*.test.js?(x)'],
+  collectCoverageFrom: [
+    'src/components//*.{js,jsx}',
+    '!src/components//*.css',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
